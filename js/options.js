b5869fd2f7c1d0c69ac46a0fb0bca854b4bb29ba
Ibrahim Ercan <ibrahim.ercan@labristeknoloji.com>
2019-04-08 19:09:03 +0300

    version 1.0

--- js/options.js
+++ js/options.js
@@ -1,11 +1,6 @@
 // Saves options to chrome.storage
 function save_options() {
-  var curlist = [];
-  $("li input[type=checkbox]").each(function(){
-      if (this.checked){ 
-          curlist.push(this.id);
-      }
-  });
+  var curlist = $(".currency_select").select2("val");
   chrome.storage.sync.set({
     selectedCurrencies: curlist
   }, function() {
@@ -25,48 +20,14 @@ function restore_options() {
   chrome.storage.sync.get({
     selectedCurrencies: ["1","2","6"]
   }, function(items) {
-       $("li input").each(function(){
-            if(items.selectedCurrencies.includes(this.id)){
-                $(this).attr("checked", "checked");
-            }
-       });
+      $('.currency_select').val(items.selectedCurrencies);
+      $('.currency_select').trigger('change');
   });
 }
-
-function filter_currency(){
-    var search_val = $("#filter_currency").val().toUpperCase();
-    var cur_val = "";
-   
-    if (search_val.length < 3){
-        $("ul li").show();
-        return;
-    }
-
-    $("ul li").each(function(){
-        cur_val = $(this).attr("title");
-        if (cur_val.includes(search_val)){
-            $(this).show()
-        }
-        else{
-            $(this).hide()
-            
-        }
-    });
-}
-
-function show_selected(){
-    $("ul li").each(function(){
-        if ($(this).find("input[type=checkbox]").checked){
-            $(this).show();
-        }
-        else{
-            $(this).hide();
-        }
-    });
-
-}
+$('.currency_select').select2();
 
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
-document.getElementById('show_selected').addEventListener('click', show_selected);
-document.getElementById('filter_currency').onkeyup=filter_currency;
+
+
+
