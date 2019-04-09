d67acd51874900be3ed33aa4113052972ccfa277
Ibrahim Ercan <ibrahim.ercan@labristeknoloji.com>
2019-04-09 13:06:45 +0300

    widget will be loaded async

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@ var url='https://www.widgets.investing.com/live-currency-cross-rates?roundedCorn
 chrome.storage.sync.get({
     selectedCurrencies: ["1","2","6"]
 }, function(items) {
-    $("#mainframe").attr("src", url + items.selectedCurrencies.join(","))
     var hg = String(items.selectedCurrencies.length * 58 + 60);
     $("#mainframe").attr("height", hg);
+    setTimeout(function(){
+        $("#mainframe").attr("src", url + items.selectedCurrencies.join(","));
+        $("#processing").hide();
+    },200);
 });
 
