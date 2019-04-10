63679e7f8f036a72cb8a5f5c3892ae4741a08313
Ibrahim Ercan <ibrahim.ercan@labristeknoloji.com>
2019-04-10 11:27:54 +0300

    async optimization

--- js/background.js
+++ js/background.js
@@ -2,8 +2,8 @@
  * Created by ibrahim on 22.05.2017.
  */
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-//TODO resize frame height
- //   chrome.tabs.create({ url: "/main.html" });
-    //$("#mainframe").attr("height", "100px" );
-});
+
+//chrome.browserAction.onClicked.addListener(function(tab) {
+//   chrome.tabs.create({ url: "/main.html" });
+//$("#mainframe").attr("height", "100px" );
+//});
--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
-var url='https://www.widgets.investing.com/live-currency-cross-rates?roundedCorners=true&theme=darkTheme&hideTitle=true&pairs=';
 
-chrome.storage.sync.get({
-    selectedCurrencies: ["1","2","6"]
-}, function(items) {
-    var hg = String(items.selectedCurrencies.length * 58 + 60);
-    $("#mainframe").attr("height", hg);
-    setTimeout(function(){
-        $("#mainframe").attr("src", url + items.selectedCurrencies.join(","));
-    },300);
-    setTimeout(function(){
-        $("#processing").hide();
-    },1000);
+document.addEventListener('DOMContentLoaded', function(){
+
+    var url='https://www.widgets.investing.com/live-currency-cross-rates?roundedCorners=true&theme=darkTheme&hideTitle=true&pairs=';
+
+    chrome.storage.sync.get({
+        selectedCurrencies: ["1","2","6"]
+    }, function(items) {
+        var hg = String(items.selectedCurrencies.length * 58 + 60);
+        $("#mainframe").attr("height", hg);
+        setTimeout(function(){
+            $("#mainframe").attr("src", url + items.selectedCurrencies.join(","));
+        },300);
+        setTimeout(function(){
+            $("#processing").hide();
+        },1000);
+    });
 });
 
 
