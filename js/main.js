d830adde3b74892775154e1de162d566c6b4cd38
Ibrahim Ercan <ibrahim.ercan@labristeknoloji.com>
2019-04-11 12:52:13 +0300

    timeout changes

--- js/main.js
+++ js/main.js
@@ -3,18 +3,18 @@ document.addEventListener('DOMContentLoaded', function(){
 
     var url='https://www.widgets.investing.com/live-currency-cross-rates?roundedCorners=true&theme=darkTheme&hideTitle=true&pairs=';
 
-    chrome.storage.sync.get({
-        selectedCurrencies: ["1","2","6"]
-    }, function(items) {
-        var hg = String(items.selectedCurrencies.length * 58 + 60);
-        $("#mainframe").attr("height", hg);
-        setTimeout(function(){
+    setTimeout(function(){
+        chrome.storage.sync.get({
+            selectedCurrencies: ["1","2","6"]
+        }, function(items) {
             $("#mainframe").attr("src", url + items.selectedCurrencies.join(","));
-        },1000);
-        setTimeout(function(){
-            $("#processing").hide();
-        },1200);
-    });
+            var hg = String(items.selectedCurrencies.length * 58 + 60);
+            $("#mainframe").attr("height", hg);
+        });
+    },800);
+    setTimeout(function(){
+        $("#processing").hide();
+    },1200);
 });
 
 
