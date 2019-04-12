3937c71c32145e83feb660f11228bf63887b117e
Ibrahim Ercan <ibrahim.ercan@labristeknoloji.com>
2019-04-12 21:14:31 +0300

    notification implemented

--- js/background.js
+++ js/background.js
@@ -1,6 +1,7 @@
 /**
  * Created by ibrahim on 22.05.2017.
  */
+
 /*
 var options = {
     type: 'basic',
@@ -11,6 +12,66 @@ var options = {
 setTimeout(function(){
     chrome.notifications.create(options);
 },2000);*/
+var notification_levels = {};
+var url = 'https://www.widgets.investing.com/live-currency-cross-rates?roundedCorners=true&theme=darkTheme&hideTitle=true&pairs=';
+
+function check_rates() {
+    chrome.storage.sync.get({
+        selectedCurrencies: ["1", "2", "6"],
+        enableNotification: true,
+        notificationThreshold: 0.5
+    }, function (items) {
+        if (!items.enableNotification) {
+            return;
+        } else {
+            var notify = false;
+            var messages = [];
+            $.get(url + items.selectedCurrencies.join(","), function (data) {
+                var html = $.parseHTML(data);
+                $.each(items.selectedCurrencies, function (index, value) {
+                    if (!(value in notification_levels)) {
+                        //initialize level to 0
+                        notification_levels[value] = 0;
+                    }
+                    var cur_pertentage = parseFloat($(html).find(".pid-" + value + "-pcp").eq(0).text());
+                    //console.log("per:" + cur_pertentage);
+                    var cur_name = $(html).find("#pair_" + value + " .js-col-pair_name a").eq(0).text();
+                    //console.log("name:" + cur_name);
+                    var cur_rate = $(html).find(".pid-" + value + "-bid").eq(0).text();
+                    //console.log("rate:" + cur_rate);
+                    if (Math.abs(cur_pertentage - notification_levels[value]) > items.notificationThreshold) {
+                        //console.log("lev: " + level + " " + notification_levels[value]);
+                        notification_levels[value] = cur_pertentage;
+                        notify = true;
+                        messages.push(cur_name + " rate is " + cur_rate + " (%" + cur_pertentage + ")");
+
+                    }
+                });
+                console.log(notification_levels);
+                if (notify === true) {
+                    var options = {
+                        type: 'basic',
+                        title: 'Currency Rates Changes',
+                        message: messages.join("\n"),
+                        iconUrl: 'css/images/icon.jpeg',
+                        requireInteraction: true
+                    };
+                    //console.log(messages);
+                    chrome.notifications.create(options);
+
+                }
+            });
+
+        }
+
+    });
+}
+
+//check rates in every 5 min
+setInterval(function () {
+    check_rates();
+}, 1000 * 5 * 60);
+
 //chrome.browserAction.onClicked.addListener(function(tab) {
 //   chrome.tabs.create({ url: "/main.html" });
 //$("#mainframe").attr("height", "100px" );
--- js/options.js
+++ js/options.js
@@ -1,27 +1,45 @@
 // Saves options to chrome.storage
 function save_options() {
   var curlist = $(".currency_select").select2("val");
+  var enable_not =  $("#enable_notification").prop("checked");
+  var threshold =  parseFloat($("#notification_threshold").val());
+  var status = $('#status');
+  if (threshold > 10 || threshold <= 0 || !threshold){
+    status.text('Error: Threshold should be between 0 and 10.');
+    return;
+  }
   chrome.storage.sync.set({
-    selectedCurrencies: curlist
+    selectedCurrencies: curlist,
+    enableNotification: enable_not,
+    notificationThreshold: threshold
   }, function() {
     // Update status to let user know options were saved.
-    var status = $('#status');
-    status.text('Options saved.');
+
+    status.text('Options saved. Window will be closed.');
     setTimeout(function() {
-      status.text('');
-    }, 5000);
+      //window.close();
+    }, 4000);
+
   });
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    selectedCurrencies: ["1","2","6"]
+    selectedCurrencies: ["1","2","6"],
+    enableNotification: true,
+    notificationThreshold: 0.5
   }, function(items) {
       $('.currency_select').val(items.selectedCurrencies);
       $('.currency_select').trigger('change');
+      if (items.enableNotification){
+        $("#enable_notification").bootstrapToggle('on');
+      }
+      else{
+        $("#enable_notification").bootstrapToggle('off');
+      }
+      $("#notification_threshold").val(items.notificationThreshold);
   });
 }
 $('.currency_select').select2();
