3586d5346aa89889a58eef93d25e7bcbf95225a9
Ibrahim Ercan <ibrahim.ercan@labristeknoloji.com>
2019-04-12 15:32:49 +0300

    notification demo added

--- js/background.js
+++ js/background.js
@@ -1,8 +1,16 @@
 /**
  * Created by ibrahim on 22.05.2017.
  */
-
-
+/*
+var options = {
+    type: 'basic',
+    title: 'Currency Changes',
+    message: 'USD/TRY rate is 6. (%1.23)',
+    iconUrl: 'css/images/icon.jpeg'
+}
+setTimeout(function(){
+    chrome.notifications.create(options);
+},2000);*/
 //chrome.browserAction.onClicked.addListener(function(tab) {
 //   chrome.tabs.create({ url: "/main.html" });
 //$("#mainframe").attr("height", "100px" );
