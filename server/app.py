908ac4f62dbd8f03d4dffd29f621e87d023df539
Ibrahim Ercan <ibrahim.metu@gmail.com>
2022-11-03 15:11:26 +0300

    exception handling on server

--- server/app.py
+++ server/app.py
@@ -10,7 +10,7 @@ CURRENCY_URL = 'https://www.investingwidgets.com/live-currency-cross-rates?round
 @app.route('/')
 def currencies():  # put application's code here
     ret_json = {}
-    pairs = request.args.get("pairs")
+    pairs = request.args.get('pairs')
     if not pairs:
         return jsonify({'error': 'Currency pairs should be provided'}), 400
 
@@ -23,34 +23,37 @@ def currencies():  # put application's code here
 
         val = redis_conn.get(curr_id)
         if val:
-            val = val.decode("utf-8")
+            val = val.decode('utf-8')
             name = val.split(':')[0]
             rate = val.split(':')[1]
             percentage = val.split(':')[2]
             ret_json[curr_id] = {
-                "name": name,
-                "rate": rate,
-                "percentage": percentage
+                'name': name,
+                'rate': rate,
+                'percentage': percentage
             }
         else:
             retrieve.append(curr_id)
 
     if retrieve:
-        html = urllib.request.urlopen(CURRENCY_URL.format(','.join(retrieve)))
-        soup = Soup(html, features="html.parser")
+        try:
+            html = urllib.request.urlopen(CURRENCY_URL.format(','.join(retrieve)))
+            soup = Soup(html, features='html.parser')
+        except:
+            return jsonify({'error': 'Could not retrieve data from investing'}), 400
         for curr_id in retrieve:
             try:
-                percentage = float(soup.select(".pid-{}-pcp".format(curr_id))[0].get_text().replace('%', ''))
+                percentage = float(soup.select('.pid-{}-pcp'.format(curr_id))[0].get_text().replace('%', ''))
                 name = soup.select('#pair_{} .js-col-pair_name a'.format(curr_id))[0].get_text()
-                rate = soup.select(".pid-{}-bid".format(curr_id))[0].get_text()
+                rate = soup.select('.pid-{}-bid'.format(curr_id))[0].get_text()
                 redis_conn.set(curr_id, '{}:{}:{}'.format(name, rate, percentage), ex=TTL)
                 ret_json[curr_id] = {
-                    "name": name,
-                    "rate": rate,
-                    "percentage": percentage
+                    'name': name,
+                    'rate': rate,
+                    'percentage': percentage
                 }
             except:
-                ret_json[curr_id] = {"name": None, "rate": None, "percentage": None}
+                ret_json[curr_id] = {'name': None, 'rate': None, 'percentage': None}
     return jsonify(ret_json)
 
 
