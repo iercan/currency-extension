a26dc605f457727b0fd059db42b787a7b3db31e0
Ibrahim Ercan <ibrahim.metu@gmail.com>
2022-11-07 13:09:00 +0300

    code clean up

--- js/main.js
+++ js/main.js
@@ -1,44 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    chrome.storage.sync.get({
+        selectedCurrencies: ["1", "2", "6"],
+        language: "www",
+        selectedCryptoCurrencies: ["945629", "1058142", "1158819"],
+        active_tab: "currency"
+    }, function (items) {
+        //console.log(items);
+        let tab = $("#" + items.active_tab + "-tab");
+        tab.attr("aria-selected", "true");
+        tab.addClass("active");
+        let tab_content = $("#" + items.active_tab);
+        tab_content.addClass("show");
+        tab_content.addClass("active");
 
-    setTimeout(function () {
-        chrome.storage.sync.get({
-            selectedCurrencies: ["1", "2", "6"],
-            language: "www",
-            selectedCryptoCurrencies: ["945629", "1058142", "1158819"],
-            active_tab: "currency"
-        }, function (items) {
-            console.log(url);
-            console.log(items);
-            var tab = $("#"+items.active_tab+"-tab");
-            tab.attr("aria-selected", "true");
-            tab.addClass("active");
-            var tab_content = $("#"+items.active_tab);
-            tab_content.addClass("show");
-            tab_content.addClass("active");
+        let mainframe = $("#mainframe");
+        if (items.selectedCurrencies.length > 0) {
+            let url = 'https://' + items.language + '.investingwidgets.com/live-currency-cross-rates?roundedCorners=true&theme=darkTheme&hideTitle=true&pairs=';
+            mainframe.attr("src", url + items.selectedCurrencies.join(","));
+        }
 
-            if(items.selectedCurrencies.length > 0){
-                var url = 'https://' + items.language + '.investingwidgets.com/live-currency-cross-rates?roundedCorners=true&theme=darkTheme&hideTitle=true&pairs=';
-                $("#mainframe").attr("src", url + items.selectedCurrencies.join(","));
-            }
-            if (items.selectedCryptoCurrencies.length > 0){
-                var crypto_url = 'https://' + items.language + '.investingwidgets.com/crypto-currency-rates?roundedCorners=true&theme=darkTheme&hideTitle=true&pairs=';
-                $("#cryptoframe").attr("src", crypto_url + items.selectedCryptoCurrencies.join(","));
-            }
+        let cryptoframe = $("#cryptoframe");
+        if (items.selectedCryptoCurrencies.length > 0) {
+            let crypto_url = 'https://' + items.language + '.investingwidgets.com/crypto-currency-rates?roundedCorners=true&theme=darkTheme&hideTitle=true&pairs=';
+            cryptoframe.attr("src", crypto_url + items.selectedCryptoCurrencies.join(","));
+        }
 
-            var hg = String(items.selectedCurrencies.length * 58 + 60);
-            $("#mainframe").attr("height", hg);
+        let hg = String(items.selectedCurrencies.length * 58 + 60);
+        mainframe.attr("height", hg);
 
-            hg = String(items.selectedCryptoCurrencies.length * 58 + 60);
-            $("#cryptoframe").attr("height", hg);
+        hg = String(items.selectedCryptoCurrencies.length * 58 + 60);
+        cryptoframe.attr("height", hg);
+    });
 
-
-
-        });
-    }, 800);
-    setTimeout(function () {
-        $(".processing").hide();
-    }, 1200);
 });
 
 
